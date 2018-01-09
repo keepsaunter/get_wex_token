b0d9c769f3646dec52ce00290df1b905f8ace047
oc <2336990515@qq.com>
2018-01-09 20:36:52 +0800

    add jsapi_ticket get

--- wex_back_task.js
+++ wex_back_task.js
@@ -3,11 +3,28 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+var wex_conf_data_file='./wex_official_config.json', wex_auth_data_file = './wex_auth_data.json', log_file='./log.txt';
 var app_id = "", app_secret="", server_port="", log_file_byte="",server_token="", expires_in=7200, has_error=false, timer_get='', notify_urls=[], set_accessToken_token='';
+
+createFile(wex_conf_data_file, '{"appId":"","appSecret":"","expires_in":0,"log_file_byte":0,"server_port":0,"server_token":"","set_accessToken_token":"","notify_urls":[]}');
+createFile(wex_auth_data_file, '{}');
+createFile(log_file);
+getConfig([timeGetData, createServer]);
+
+//创建文件
+function createFile(file_path, init_content){
+	if(!fs.existsSync(file_path)){
+		fs.writeFile(file_path, init_content?init_content:'', function (err) {
+			if(err){
+				console.log("创建数据文件失败！");
+			}
+		});
+	}
+}
 
 //读取配置文件
 function getConfig(callback){
-	fs.readFile('./wex_official_config.json', function(err, buffer){
+	fs.readFile(wex_conf_data_file, function(err, buffer){
 		if(err){
 			console.log("读取配置失败！");
 			console.log(err);
@@ -35,59 +52,83 @@ function getConfig(callback){
 		}
 	})
 }
+
 //用https请求微信服务器
-function requestAccessToken(){
-	var get_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+app_id+"&secret="+app_secret;
-	
-	https.get(get_url, (res) => {
+function requestWex(){
+	var get_access_token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+app_id+"&secret="+app_secret;
+	var get_jsapi_ticket_url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=";
+
+	https.get(get_access_token_url, (res) => {
 		res.on('data', (chunk) => {
-			var remote_expires_in = 0;
 			var get_data = JSON.parse(chunk);
-			try{
-				var remote_expires_in = get_data.expires_in;
-			}catch(e){
+			if(!get_data.errcode){
+				https.get(get_jsapi_ticket_url+get_data.access_token, (res) => {
+					res.on('data', (chunk) => {
+						var data = JSON.parse(chunk);
+						if(!data.errcode || data.errcode == 0){
+							get_data.ticket = data.ticket;
+							get_data.expires_in = Math.min(get_data.expires_in, data.expires_in);
+							saveRecData(get_data);
+						}else{
+							writeLog("获取jsapi_ticket失败："+data.errmsg+"！");
+							has_error = true;
+						}
+					})
+				});
+			}else{
+				writeLog("获取access_token失败："+get_data.errmsg+"！");
 				has_error = true;
-				return;
 			}
-			if(expires_in < remote_expires_in-5 || expires_in > remote_expires_in+5){
-				expires_in = remote_expires_in;
-				updateFile('./wex_official_config.json', {expires_in: remote_expires_in}, function(err){
-					if(err){
-						console.log("写入配置文件失败！");
-						console.error(err);
-						has_error = true;
-					}else{
-						// console.log('写入配置文件成功！');
-					}
-				})
-			}
-			updateFile('./wex_auth_data.json', get_data, function(err){
-				if(err) {
-			   		console.log('写入access_token失败！');
-			    	console.error(err);
-			    	has_error = true;
-			    } else {
-			    	// console.log('写入access_token成功！');
-			    	notifyOther(get_data.access_token);
-			    }
-			})
 		})
 	});
 }
 
+//处理接收到的数据
+function saveRecData(data){
+	if(data && typeof data == 'object'){
+		var remote_expires_in = data.expires_in || expires_in;
+		if(expires_in < remote_expires_in-5 || expires_in > remote_expires_in+5){
+			expires_in = remote_expires_in;
+			updateFile(wex_conf_data_file, {expires_in: remote_expires_in}, function(err){
+				if(err){
+					console.log("写入配置文件失败！");
+					console.error(err);
+					has_error = true;
+				}else{
+					// console.log('写入配置文件成功！');
+				}
+			})
+		}
+		updateFile(wex_auth_data_file, data, function(err){
+			if(err) {
+		   		console.log('写入数据文件失败！');
+		    	console.error(err);
+		    	has_error = true;
+		    } else {
+		    	// console.log('写入数据文件成功！');
+		    	delete data.expires_in;
+		    	notifyOther(data);
+		    }
+		})
+	}else{
+		has_error = true;
+		return;
+	}
+}
+
 //设置定时器
-function timeGetAccessToken(){
+function timeGetData(){
 	if(timer_get){
 		clearInterval(timer_get);
 	}
-	requestAccessToken();
+	requestWex();
 	
 	timer_get = setInterval(function(){
-		requestAccessToken();
+		requestWex();
 	}, (expires_in - 8) * 1000);
 }
 
-//创建node服务用于强制刷新token
+//创建node服务用于强制刷新数据或重置配置文件
 function createServer(){
 	var run_port = process.env.WEX_DATA_PORTf || server_port;	//用于运行时设置端口号
 	var server = http.createServer(function(req, res){
@@ -105,10 +146,10 @@ function createServer(){
 
 		if(req_token && req_token[1] === server_token){
 			if(path_name == "/"){
-				timeGetAccessToken(expires_in);
+				timeGetData(expires_in);
 				res_data.data = "wex数据文件已更新!";
 			}else if(path_name == "/resetConf"){
-				getConfig(timeGetAccessToken);
+				getConfig(timeGetData);
 				res_data.data = "重置配置成功!";
 			}else{
 				res_data = {st: 404, data:"非法请求!"}
@@ -136,10 +177,11 @@ function getResponseContent(data, type){
 }
 
 //更新access_token时通知变化
-function notifyOther(new_access_token){
+function notifyOther(json_data){
 	notify_urls.forEach((val)=>{
 		try{
-			var req = http.get(val+"?set_accessToken_token="+new Buffer(set_accessToken_token).toString('base64')+"&new_access_token="+new Buffer(new_access_token).toString('base64'), (res)=>{
+			var req = http.get(val+"?set_accessToken_token="+new Buffer(set_accessToken_token).toString('base64')
+				+"&new_data="+new Buffer(JSON.stringify(json_data)).toString('base64'), (res)=>{
 				res.setEncoding('utf-8');
 				res.on("data", (chunk)=>{
 					try{
@@ -155,7 +197,7 @@ function notifyOther(new_access_token){
 				writeLog(val+"\t同步通知失败!");
 			})
 		}catch(e){
-			
+			// console.log(e);
 		}
 	})
 }
@@ -184,7 +226,6 @@ function updateFile(file_path, data, callback){
 
 //写入log；大于log_file_byte字节时自动清除
 function writeLog(data){
-	var log_file = './log.txt';
 	var date_time = new Date().toLocaleString();
 	fs.readFile(log_file,'utf-8', function(err, buffer){
         if(err){
@@ -212,5 +253,3 @@ function writeLog(data){
         }
 	})
 }
-
-getConfig([timeGetAccessToken, createServer]);
